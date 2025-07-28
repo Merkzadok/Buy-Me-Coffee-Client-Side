69d8f23f7a49a938b172f527acd240c7a4baa551
25LP3742 25LP3742 <Zoljargal.g247@gmail.com>
2025-07-28 14:20:57 +0800

    donation profile

---
+++ src/app/(donation)/components/Donation-SocialMedia.tsx
@@ -0,0 +1,10 @@
+export const DonationSocialMedia = () => {
+    return (
+        <div>
+            <div>
+                <h1 className="font-semibold">Social media URL</h1>
+                <h2 className="font-normal">https://buymeacoffee.com/spacerulz44</h2>
+            </div>
+        </div>
+    )
+}
--- src/app/(donation)/page.tsx
+++ src/app/(donation)/page.tsx
@@ -1,11 +1,13 @@
 import { DonationBackground } from "./components/Donation-Background";
 import { DonationProfile } from "./components/Donation-Profile";
+import { DonationSocialMedia } from "./components/Donation-SocialMedia";
 
 export default function Donation() {
   return (
     <div>
       <DonationBackground/>
       <DonationProfile/>
+      <DonationSocialMedia/>
     </div>
   );
 }
