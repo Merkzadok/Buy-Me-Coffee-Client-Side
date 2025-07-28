9ba1228f5c9e8258a38b508769b62323b9d9f52f
25LP3402 25LP3402 <boloroots47@gmail.com>
2025-07-28 12:20:36 +0800

    fixed conflictg

---
+++ src/app/(auth)/create-profile/_components/CreateProfile.tsx
@@ -0,0 +1,9 @@
+export const CreateProfile = () => {
+  return (
+    <div>
+      <p>Complete your profile page</p>
+      <p>add photo</p>
+      
+    </div>
+  );
+};
---
+++ src/app/(auth)/create-profile/page.tsx
@@ -0,0 +1,10 @@
+import { CreateProfile } from "./_components/CreateProfile";
+
+
+export default function page() {
+  return (
+    <div>
+      <CreateProfile/>
+    </div>
+  );
+}
