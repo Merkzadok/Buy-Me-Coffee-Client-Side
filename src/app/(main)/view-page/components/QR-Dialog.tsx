e704143ecf654c5266babd267415dc794f7c7be3
25LP3742 25LP3742 <Zoljargal.g247@gmail.com>
2025-08-01 12:41:56 +0800

    qr

---
+++ src/app/(main)/view-page/components/QR-Dialog.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function QRdialog() {
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button variant="secondary" className="w-full">Support</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle className="m-auto">Scan QR code</DialogTitle>
+            <DialogDescription className="m-auto">
+             Scan the QR code to complete your donation
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <div className="grid gap-3">
+              <Label htmlFor="name-1">Name</Label>
+              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="username-1">Username</Label>
+              <Input id="username-1" name="username" defaultValue="@peduarte" />
+            </div>
+          </div>
+        </DialogContent>
+      </form>
+    </Dialog>
+  )
+}
