e36e4f577ed9d5407bf52894db294fbdcad896b5
Bolormaa <boloroots47@gmail.com>
2025-08-04 23:02:00 +0800

    explore view page user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 "use client"
 
 import { Sidebar } from "@/components/Sidebar";
-import { UserContext } from "@/provider/currentUserProvider";
-import { useContext } from "react";
+
 
 export default function Home() {
-
-  const context = useContext(UserContext)
-  console.log("CONTEXT:", context);
   
   return (
     <div className="flex gap-10 justify-center">
