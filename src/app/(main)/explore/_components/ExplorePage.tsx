a999a626a4d4ee8b06a36991f0db869f4103d79d
25LP3402 25LP3402 <boloroots47@gmail.com>
2025-08-08 09:44:07 +0800

    pagination done

--- src/app/(main)/explore/_components/ExplorePage.tsx
+++ src/app/(main)/explore/_components/ExplorePage.tsx
@@ -6,18 +6,25 @@ import { NoUser } from "./NoUsers";
 import { UserSearchInput } from "./UserSearchInput";
 
 import axios from "axios";
+import { parseAsInteger, useQueryState } from "nuqs";
+
 import { Button } from "@/components/ui/button";
 import { ProfileWithUserNameType } from "@/types/DonationType";
+import { number } from "zod";
 
 type exploreType = {
   usersProfile: ProfileWithUserNameType[];
   totalUsers: number;
-  totalPage:number
+  totalPage: number;
 };
 
 export const ExplorePage = () => {
   const [users, setUsers] = useState<null | exploreType>(null);
-  const [page, setPage] = useState<number>(1);
+
+  const [page, setPage] = useQueryState<number>(
+    "page",
+    parseAsInteger.withDefault(1)
+  );
 
   const getExplore = async () => {
     try {
@@ -41,6 +48,16 @@ export const ExplorePage = () => {
   }, [page]);
   console.log("page:", page);
 
+  const totalPage = users?.totalPage || 1;
+
+  const arr = Array(totalPage - 1)
+    .fill(null)
+    .map((_, index) => index + 1);
+
+  const handleSelect = (huudas: number) => {
+    setPage(huudas);
+  };
+
   return (
     <div className=" py-10 flex flex-col gap-6">
       <p className="font-semibold text-xl">Explore creators</p>
@@ -60,10 +77,30 @@ export const ExplorePage = () => {
         </div>
       )}
 
-      <div>
+      <div className="mt-5 flex justify-end gap-5">
         <Button onClick={() => setPage(page - 1)} disabled={page === 1}>
           Previous
         </Button>
+        <div className="flex gap-2">
+          {arr.length > 0 &&
+            arr.map((number, i) => {
+              if (page + 1 >= number && page - 1 < number) {
+                return (
+                  <Button
+                    onClick={() => handleSelect(number)}
+                    variant="outline"
+                    key={i}
+                  >
+                    {number}
+                  </Button>
+                );
+              }
+            })}
+
+          <Button onClick={() => handleSelect(totalPage)} variant="outline">
+            {totalPage}
+          </Button>
+        </div>
         <Button
           onClick={() => setPage(page + 1)}
           disabled={page === users?.totalPage}
